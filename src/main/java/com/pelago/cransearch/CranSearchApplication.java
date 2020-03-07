52fc092d26fcf34303920b8aca6d893c98f6fd97
Truong Viet Trung <viettrung9012@yahoo.com>
2020-03-07 11:22:03 +0800

    init: spring-boot project

---
+++ src/main/java/com/pelago/cransearch/CranSearchApplication.java
@@ -0,0 +1,13 @@
+package com.pelago.cransearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CranSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CranSearchApplication.class, args);
+	}
+
+}
